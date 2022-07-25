03f14ba9e54e48a17b4581e7c3e7f5fda7c447d6
Fratboychad22 <alimacky888@outlook.com>
2022-07-25 10:11:59 -0700

    Added 8 Ball and Tanzanite Ender Pearl

--- src/main/java/net/fratboychad21/tutorialmod/item/ModItems.java
+++ src/main/java/net/fratboychad21/tutorialmod/item/ModItems.java
@@ -2,6 +2,8 @@ package net.fratboychad21.tutorialmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fratboychad21.tutorialmod.TutorialMod;
+import net.fratboychad21.tutorialmod.item.custom.EightBallItem;
+import net.fratboychad21.tutorialmod.item.custom.TanzaniteEnderPearlItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,6 +15,12 @@ public class ModItems {
 
     public static final Item TANZANITE = regiseterItem("tanzanite",
             new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
+
+    public static final Item EIGHT_BALL = regiseterItem("eight_ball",
+            new EightBallItem(new FabricItemSettings().group(ModItemGroup.TANZANITE).maxCount(1)));
+
+    public static final Item TANZANITE_ENDER_PEARL = regiseterItem("tanzanite_ender_pearl",
+            new TanzaniteEnderPearlItem(new FabricItemSettings().group(ModItemGroup.TANZANITE).maxCount(1)));
 
 
     private static Item regiseterItem(String name, Item item) {
